efa91d88de95c9171723fea723b66f929a628b71
Mark <foratik2001@gmail.com>
2020-04-15 18:59:32 +0700

    first commid

---
+++ sss.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/*var a = 1;
+let second = 2;
+const pi = 3.14;
+console.log(a);
+let person = {
+    name: "John",
+    age: 12,
+    isMarried: false
+};
+console.log(person.age);
+let arr = ['lun', 'sds', 'nmp'];
+console.log(arr[1]);
+//alert("Hello daun");
+let answer = +prompt("Сколько вам лет?");
+//console.log(answer);
+console.log(typeof(answer));
+
+console.log("arr" + "- obect");
+console.log(4+ +" - obhect");*/
+let incr = 10, 
+    dectr = 10;
+incr++;
+dectr--;   
+console.log(--incr); 
+console.log(++dectr);
+console.log(5%2);
